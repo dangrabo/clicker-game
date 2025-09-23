d19b22e8c1514077cc2aef8b8ed216d5f865a2e4
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-09-23 14:04:15 -0700

    added fucntionality for upgrade

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const button = document.querySelector('#clicker');
+const clicker = document.querySelector('#clicker');
 const displayedScore = document.querySelector('#score');
 
+// Increment upgrade
+const incrementUpgrade = document.querySelector('#upgrade-1');
+let incrementCost = 10;
+
+
 let score = 0
-const incrementor = 1;
+let incrementor = 1;
 
-const upgradeOne = {
-    cost: 10,
-    
-}
-
-button.onclick = () => {
-    score++;
+// Main clicker functionality
+clicker.onclick = () => {
+    score += incrementor;
     displayedScore.textContent = score;
 }
 
+// Increment Upgrade functionality
+incrementUpgrade.onclick = () => {
+    if (score < incrementCost) {
+        alert('You do not have enough points to purchase this upgrade');
+        return;
+    }
+    incrementor++;
+    score -= incrementCost;
+    incrementCost *= 2;
+    alert("Purchase successful :D");
+    updateScore();
+}
 
+const updateScore = () => {
+    displayedScore.textContent = score;
+}
