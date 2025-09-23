ead38e60f6a02293981392c610a5590e06d83a8a
dangrabo <193651636+dangrabo@users.noreply.github.com>
2025-09-23 14:48:30 -0700

    pulling

--- script.js
+++ script.js
@@ -7,10 +7,17 @@ const displayedScore = document.querySelector('#score');
 
 // Increment upgrade
 const incrementUpgradeBtn = document.querySelector('#upg-1-btn');
-const incrementUpgradeSpan = document.querySelector('#upg-1-cost');
+const incrementUpgradeCost = document.querySelector('#upg-1-cost');
 let incrementCost = 10;
 
+// Time upgrade
+const timeUpgradeBtn = document.querySelector('#upg-2-btn');
+const timeUpgradeCost = document.querySelector('#upg-2-cost');
+const timeUpgradeReward = document.querySelector('#time-plus');
+let timeCost = 10;
+let timeIncrementor = 10;
 
+// General player variablese
 let score = 0
 let incrementor = 1;
 
@@ -20,7 +27,7 @@ clicker.onclick = () => {
     displayedScore.textContent = score;
 }
 
-// Increment Upgrade functionality
+// Increment Upgrade functionality (upg-1)
 incrementUpgradeBtn.onclick = () => {
     if (score < incrementCost) {
         alert('You do not have enough points to purchase this upgrade');
@@ -33,7 +40,25 @@ incrementUpgradeBtn.onclick = () => {
     updatePage();
 }
 
+// Time upgrade funcitonality (upg-2)
+const giveTimedReward = () => {
+    score += timeIncrementor;
+    updatePage();
+}
+
+setInterval(giveTimedReward, 10000);
+
+// Time upgrade btn fucntionality
+timeUpgradeBtn.onclick = () => {
+    if (score < timeCost) {
+        alert('You do not have enough points to purchase this upgrade');
+        return;
+    }
+}
+
 const updatePage = () => {
     displayedScore.textContent = score;
-    incrementUpgradeSpan.textContent = incrementCost;
+    incrementUpgradeCost.textContent = incrementCost;
+    timeUpgradeCost.textContent = timeCost;
+    timeUpgradeReward.innerHTML = timeIncrementor
 }
